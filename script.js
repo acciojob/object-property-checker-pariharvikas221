11233a4e29cb818c98da9000dddb753de1c2cf36
pariharvikas221 <117854792+pariharvikas221@users.noreply.github.com>
2023-06-03 16:51:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
 
 function hasKey(key) {
 	for(let i in sampleObject){
-		if(sampleObject[i] == key){
+		if(sampleObject[i] === key){
 			return true;
 		}
 	}
@@ -13,3 +13,4 @@ function hasKey(key) {
 // Do not change the code below
 const key = prompt("Enter Key.");
 alert(hasKey(key));
+
